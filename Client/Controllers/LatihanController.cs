993c13917a4f71fe88fb3d6baab87b30b096069c
surya994 <muhammadsuryanto1111@gmail.com>
2022-04-04 16:11:38 +0700

    Add CSS and JS

---
+++ Client/Controllers/LatihanController.cs
@@ -0,0 +1,26 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class LatihanController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LatihanController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const animals = [
+    { name: "garfield", species: "cat", class: { name: "mamalia" } },
+    { name: "nemo", species: "fish", class: { name: "invertebrata" } },
+    { name: "tom", species: "cat", class: { name: "mamalia" } },
+    { name: "garry", species: "cat", class: { name: "mamalia" } },
+    { name: "dory", species: "fish", class: { name: "invertebrata" } },
+    { name: "oren", species: "cat", class: { name: "mamalia" } },
+    { name: "meong", species: "cat", class: { name: "mamalia" } },
+    { name: "lele", species: "fish", class: { name: "invertebrata" } }
+
+]
+const onlyCat = [];
+for (var i = 0; i < animals.length; i++) {
+    if (animals[i].species=="cat") {
+        onlyCat.push(animals[i])
+    }
+}
+console.log(onlyCat);
+
+for (var i = 0; i < animals.length; i++) {
+    if (animals[i].species == "fish") {
+        animals[i].class.name = "non mamalia";
+    }
+}
+console.log(animals);
